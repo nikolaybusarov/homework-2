bfb11384ad9132fae599059b77295625fe7aaf73
nikolaybusarov <56512128+nikolaybusarov@users.noreply.github.com>
2019-11-22 22:15:26 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,35 +5,27 @@ import java.util.Random;
 
 public class Main {
 
+    public static final Random RANDOM = new Random();
 
     public static void main(String[] args) {
-        System.out.println("Задача 1");
-        System.out.println(Polindrome("aBBa"));
-        System.out.println("Задача 2 ");
-        RandomArrayNumber(new int[5]);
-        System.out.println("Задача 3");
-        System.out.println(NumberPosition(new int[5], 2));
-        System.out.println("Задача 4");
-        MinMaxElement(new int[]{1, 3, 8, -7, -3});
-        System.out.println("Задача 5");
-        System.out.println(AverageNumber(0));
-        System.out.println("Задача 6");
-        NumberOfNegativeElements(new Random());
-        System.out.println("Задача 8");
-        ReverseArrayOrder(0);
-        System.out.println("Задача 9");
-        System.out.println(ArrayCheck(new int[10]));
-        System.out.println("Задача 10");
-        System.out.println(Arrays.toString(ShuffleArrayElements(new int[]{1, 2, 3, 4, 5, 6})));
-        System.out.println("Задача 11");
-        SortingAnArrayInAscendingOrder(new int[]{5, 8, 6, -3, 1, -7, 0, -4});
-        System.out.println("Задача 12");
-        int[][] array = RandomTwoDimensionalArray(5, 5);
+        System.out.println(polindrome("aBBa"));
+        int [] array  = randomArray(10);
+        System.out.println(Arrays.toString(array));
+        System.out.println(numberPosition(5,array));
+        System.out.println(Arrays.toString(minMaxElement(array)));
+        System.out.println(averageNumber(0,array));
+        System.out.println(Arrays.toString(numberOfNegativeElements(array)));
+        System.out.println(Arrays.toString(deleteElement(3,array)));
+        System.out.println(Arrays.toString(reverseArrayOrder(array)));
+        System.out.println(Arrays.toString(arrayCheck(array)));
+        System.out.println(Arrays.toString(shuffleArrayElements(array)));
+        System.out.println(Arrays.toString(sortingAnArrayInAscendingOrder(array)));
+        System.out.println(randomTwoDimensionalArray(5, 5));
 
 
     }
 
-    public static boolean Polindrome(String name) {
+    public static boolean polindrome (String name) {
         int numbers = name.length();
         for (int i = 0; i < (numbers / 2); i++) {
             if (name.charAt(i) != name.charAt(numbers - i - 1)) {
@@ -44,19 +36,18 @@ public class Main {
 
     }
 
-    public static int RandomArrayNumber(int array[]) {
+    public static int[] randomArray (int x) {
+        int array [] = new int[x];
         for (int i = 0; i < array.length; i++) {
-            array[i] = ((int) (Math.random() * 10));
-            System.out.println(array[i]);
+            array[i] = RANDOM.nextInt(21)-5 ;
         }
-        return 0;
+        return array;
     }
 
-    public static int NumberPosition(int array[], int x) {
+
+    public static int numberPosition (int x, int array []) {
         for (int i = 0; i < array.length; i++) {
-            array[i] = ((int) (Math.random() * 10));
-            System.out.println(array[i]);
-            if (x == array[i]) {
+            if (array[i] == x) {
                 return i;
             }
         }
@@ -64,61 +55,79 @@ public class Main {
 
     }
 
-    public static int MinMaxElement(int array[]) {
-        int min, max;
-        min = max = array[0];
-        for (int i = 0; i < 5; i++) {
-            if (array[i] < min) min = array[i];
-            if (array[i] > max) max = array[i];
+    public static int[] minMaxElement (int array []) {
+        int max = array[0];
+        int imax = 0;
+        int min = array[0];
+        int imin = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                imax = i;
+            }
+            if (array[i] < min) {
+                min = array[i];
+                imin = i;
+            }
         }
-        System.out.println("min=" + min + " max=" + max);
-        return min;
+        array[imin] = max;
+        array[imax] = min;
+        return array;
     }
 
-    public static double AverageNumber(double average) {
-        int[] numbers = {10, 20, 30, 40};
-        if (numbers.length > 0) {
+    public static double averageNumber (double x,int array []) {
+        if (array.length > 0) {
             double sum = 0;
-            for (int i = 0; i < numbers.length; i++) {
-                sum += numbers[i];
+            for (int i = 0; i < array.length; i++) {
+                sum += array[i];
             }
-            average = sum / numbers.length;
+            x = sum / array.length;
             System.out.print("среднее арифмитическое значение = ");
         }
-        return average;
+        return x;
 
     }
 
-    public static int NumberOfNegativeElements(Random random) {
-        int a = random.nextInt(10) + 1;
-        int[] numbers = new int[10];
-        for (int i = 0; i < numbers.length; i++) numbers[i] = random.nextInt(21) - 10;
-        System.out.println(Arrays.toString(numbers));
+    public static int[] numberOfNegativeElements ( int array []) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = RANDOM.nextInt(21) - 10;
+        }
         int count = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] < 0) count++;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < 0) count++;
         }
         System.out.println("в массиве " + count + "  отрицательных элемента (-ов) ");
-        return a;
+
+        return array;
     }
 
-
-    public static int ReverseArrayOrder(int lengs) {
-        int[] array = new int[]{1, 2, 3, 4, 5};
-        for (int i : array) {
-            lengs++;
-        }
-        for (int i = lengs - 1; i >= 0; i--) {
-            System.out.println(array[i]);
-        }
-        return lengs;
-    }
-
-    public static int ArrayCheck (int array[]) {
+    public static int[] deleteElement (int n ,int array []) {
+            int[] arrayElement = new int[array.length - 1];
+        int j = 0;
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * 20);
+            if (i != n) {
+                arrayElement[j] = array[i];
+                j++;
+            }
         }
-        System.out.println(Arrays.toString(array));
+        return arrayElement;
+    }
+
+
+    public static int[] reverseArrayOrder (int array []) {
+        int[] reverseArray = new int[array.length];
+        int j = array.length - 1;
+        for (int i = 0; i < array.length; i++) {
+            reverseArray[j] = array[i];
+            j--;
+        }
+        return reverseArray;
+    }
+
+    public static int[] arrayCheck (int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (int) (Math.random() * 20 );
+        }
         boolean x = true;
         for (int i = 1; i < array.length; i++) {
             if (array[i] <= array[i - 1]) {
@@ -131,14 +140,13 @@ public class Main {
         } else {
             System.out.println("Массив не отсортирован  по возрастанию");
         }
-        return 0;
+        return array;
     }
 
 
-    public static int[] ShuffleArrayElements(int[] array) {
-        Random random = new Random();
+    public static int[] shuffleArrayElements (int [] array) {
         for (int i = array.length - 1; i > 0; i--) {
-            int index = random.nextInt(i + 1);
+            int index = RANDOM.nextInt(i + 1);
             int a = array[index];
             array[index] = array[i];
             array[i] = a;
@@ -146,40 +154,31 @@ public class Main {
         return array;
     }
 
-    public static int SortingAnArrayInAscendingOrder(int array[]) {
-        int a, b, t;
-        int size;
-        size = 8;
-
-        for (a = 1; a < size; a++)
-            for (b = size - 1; b >= a; b--) {
-                if (array[b - 1] > array[b]) {
-                    t = array[b - 1];
-                    array[b - 1] = array[b];
-                    array[b] = t;
+    public static int[] sortingAnArrayInAscendingOrder (int array[]) {
+        for (int j = 0; j < array.length; j++) {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    int tmp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = tmp;
                 }
             }
-        System.out.println("отсортированный массив: ");
-        for (int i = 0; i < size; i++) {
-            System.out.print("" + array[i]);
-            System.out.println();
         }
-        return a;
+        return array;
     }
 
-    public static int[][] RandomTwoDimensionalArray(int length, int width) {
-        int[][] array = new int[length][width];
-        Random random = new Random();
+    public static int[][] randomTwoDimensionalArray (int length, int width) {
+        int[][] array2 = new int[length][width];
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < width; j++) {
-                array[i][j] = random.nextInt(100);
-                System.out.print(array[i][j] + " ");
+                array2[i][j] = RANDOM.nextInt(100) ;
+                System.out.print(array2[i][j] + " ");
                 if (j == (width - 1)) {
                     System.out.println();
                 }
             }
         }
-        return array;
+        return array2;
     }
 
 }
